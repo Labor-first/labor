96db575c6a58a0650063184e4520387c5895528b
lixin9240 <3258599349@qq.com>
2026-03-22 19:46:47 +0800

    Update database.php

--- laborary/config/database.php
+++ laborary/config/database.php
@@ -59,8 +59,12 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+    // PHP 8.5+ 使用新常量，旧版本使用旧常量
+    PHP_VERSION_ID >= 80500 
+        ? \Pdo\Mysql::ATTR_SSL_CA 
+        : PDO::MYSQL_ATTR_SSL_CA 
+    => env('MYSQL_ATTR_SSL_CA'),
+]) : [],
         ],
 
         'mariadb' => [
